93a007f1469533b6e0aec092bf2db6da29813471
Christian Juschin <cjuschin@gmail.com>
2019-04-17 15:34:01 +0200

    portfolio

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,14 @@ document.getElementById("button1").addEventListener("click",
 	function(){
 		document.querySelector(".bg-modal").style.display = "flex";
 		document.querySelector(".row2").style.display = "flex";
+		document.querySelector(".row").style.display = "none";
 	});
 
 document.querySelector(".close").addEventListener("click",
 	function(){
 		document.querySelector(".bg-modal").style.display = "none";
 		document.querySelector(".row2").style.display = "none";
+		document.querySelector(".row").style.display = "flex";
 	});
 
 
@@ -16,12 +18,14 @@ document.getElementById("button2").addEventListener("click",
 	function(){
 		document.querySelector(".bg-modal").style.display = "flex";
 		document.querySelector(".row3").style.display = "flex";
+		document.querySelector(".row").style.display = "none";
 	});
 
 document.querySelector(".close2").addEventListener("click",
 	function(){
 		document.querySelector(".bg-modal").style.display = "none";
 		document.querySelector(".row3").style.display = "none";
+		document.querySelector(".row").style.display = "flex";
 	});
 
 
@@ -30,10 +34,12 @@ document.getElementById("button3").addEventListener("click",
 	function(){
 		document.querySelector(".bg-modal").style.display = "flex";
 		document.querySelector(".row4").style.display = "flex";
+		document.querySelector(".row").style.display = "none";
 	});
 
 document.querySelector(".close3").addEventListener("click",
 	function(){
 		document.querySelector(".bg-modal").style.display = "none";
 		document.querySelector(".row4").style.display = "none";
+		document.querySelector(".row").style.display = "flex";
 	});
